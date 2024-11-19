0168ac8a9ad11cc654fe5b55f6cb360932f549e1
tensei <908739+tnsicdr@users.noreply.github.com>
2024-11-19 14:49:17 -0500

    feat: build layout

--- src/shared/i18n/ui.ts
+++ src/shared/i18n/ui.ts
@@ -7,6 +7,7 @@ export const defaultLang = "en";
 export const ui = {
   en: {
     siteTitle: `tnsiWEB`,
+    "footer.neocities": "Hosted by Neocities",
     "nav.about": `About`,
     "nav.home": `Home`,
   },
